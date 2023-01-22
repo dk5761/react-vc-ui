5a8286e85f2fde8104464ebaa3d304a04a2ced58
Darshan Kadam <32063387+dk5761@users.noreply.github.com>
2023-01-22 23:25:22 +0530

    added hosted server

---
+++ src/context/SocketContext.tsx
@@ -0,0 +1,171 @@
+import React, { createContext, useState, useRef, useEffect } from 'react'
+import { io } from 'socket.io-client';
+import Peer from 'simple-peer'
+
+
+interface ISocketContext {
+    call: ICall | undefined,
+    callAccepted: boolean,
+    myVideo: React.MutableRefObject<HTMLVideoElement | null>,
+    userVideo: React.MutableRefObject<HTMLVideoElement | null>,
+    stream: MediaStream | undefined,
+    name: string,
+    setName: React.Dispatch<React.SetStateAction<string>>,
+    callEnded: boolean,
+    me: string | undefined,
+    callUser: (id: string) => void,
+    leaveCall: () => void,
+    answerCall: () => void,
+}
+
+const SocketContext = createContext({} as ISocketContext);
+
+const socket = io('https://vc-server-dk5761.onrender.com');
+
+interface IContextProvider {
+    children: React.ReactNode
+}
+
+interface ICall {
+    from: string,
+    isReceivingCall: boolean,
+    name: string,
+    signal: Peer.SignalData
+}
+
+const ContextProvider = ({
+    children
+}: IContextProvider) => {
+
+    const [stream, setStream] = useState<MediaStream>()
+    const [me, setMe] = useState<string>("")
+    const [call, setCall] = useState<ICall>()
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+    const [name, setName] = useState<string>("");
+
+
+
+    const myVideo = useRef<HTMLVideoElement | null>(null);
+    const userVideo = useRef<HTMLVideoElement | null>(null);
+    const connectionRef = useRef();
+
+
+    useEffect(() => {
+        // get the user media device access.
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then((currentStream) => {
+            setStream(currentStream);
+
+            if (myVideo.current) {
+                myVideo.current.srcObject = currentStream
+            }
+        })
+
+        socket.on('me', (id) => setMe(id));
+        socket.on('callUser', ({ from, name: callerName, signal }) => {
+            setCall({
+                isReceivingCall: true,
+                from,
+                name: callerName,
+                signal
+            })
+        })
+    }, [])
+
+
+    const answerCall = () => {
+        setCallAccepted(true);
+        const peer = new Peer({
+            initiator: false, trickle: false, stream
+        });
+
+        peer.on('signal', (data) => {
+            socket.emit('answerCall', {
+                signal: data,
+                to: call?.from
+            })
+        });
+
+        peer.on('stream', (currentStream) => {
+            if (userVideo.current) {
+                userVideo.current.srcObject = currentStream
+            }
+        });
+
+        if (call) {
+            peer.signal(call.signal)
+        };
+
+        // @ts-ignore 
+        connectionRef.current = peer
+
+    }
+
+    const callUser = (id: string) => {
+        const peer = new Peer({
+            initiator: true, trickle: false, stream
+        });
+
+        peer.on('signal', (data) => {
+            socket.emit('callUser', {
+                userToCall: id,
+                signalData: data,
+                from: me,
+                name: name
+            })
+        });
+
+        peer.on('stream', (currentStream) => {
+            if (userVideo.current) {
+                userVideo.current.srcObject = currentStream
+            }
+        });
+
+        socket.on('callAccepted', (signal) => {
+            setCallAccepted(true);
+
+            peer.signal(signal);
+        })
+        // @ts-ignore 
+        connectionRef.current = peer
+    }
+
+    const leaveCall = () => {
+        setCallEnded(true);
+        // @ts-ignore 
+        connectionRef.current!.destroy();
+
+        window.location.reload()
+    }
+
+
+
+
+    return (
+        <SocketContext.Provider value={{
+            call,
+            callAccepted,
+            myVideo,
+            userVideo,
+            stream,
+            name,
+            setName,
+            callEnded,
+            me,
+            callUser,
+            leaveCall,
+            answerCall,
+        }}>
+            {
+                children
+            }
+        </SocketContext.Provider>
+    )
+}
+
+export {
+    ContextProvider, SocketContext
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material'
+import { ContextProvider } from './context/SocketContext'
 
+const theme = createTheme({})
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
